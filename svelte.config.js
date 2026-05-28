8389be3ca13d5287ef8179e00c1766128d611fe0
Admin <minhh.notminh@gmail.com>
2026-05-28 15:03:00 +0700

    pull request for team list

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-		
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
+
 	compilerOptions: {
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+		runes: ({ filename }) =>
+			filename.split(/[/\\]/).includes('node_modules')
+				? undefined
+				: true
 	},
+
 	kit: {
 		adapter: adapter({ out: 'build/sveltekit' }),
 		alias: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,6 @@
-import { paraglideVitePlugin } from '@inlang/paraglide-js';
-import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit(),
-		paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' })
-	],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://localhost:3000',
-				changeOrigin: true
-			}
-		}
-	}
+	plugins: [sveltekit()]
 });
